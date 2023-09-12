93709a94cd8428afdb3e02c8623680b53be23794
Duong Vo <duong.vo@evoluter.com>
2023-09-12 09:49:52 +0700

    add jenkin file

--- src/main/java/org/tribe_sdk/service/TribeSDKServiceImpl.java
+++ src/main/java/org/tribe_sdk/service/TribeSDKServiceImpl.java
@@ -280,6 +280,5 @@ public class TribeSDKServiceImpl implements TribeSDK {
             return new TribeSDKServiceImpl(this.dbUrl, this.dbUsername, this.dbPassword, this.tribeProgramUrl, this.accessKey, this.accessApiId, this.accessApiKey, this.dataEncryptionKey, this.version, this.privateKey, this.publicKey, this.encryptMode, this.pepSanctionUrl, this.pepAccessToken, this.pepAuthToken);
         }
 
-
     }
 }
